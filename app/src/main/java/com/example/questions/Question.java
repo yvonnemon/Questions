865e39fab887da2e726d173828d6ff1aa0b3b17e
Yvonne <yvosobrero2@hotmail.com>
2024-10-23 17:18:46 +0200

    Initial commit

---
+++ app/src/main/java/com/example/questions/Question.java
@@ -0,0 +1,23 @@
+package com.example.questions;
+
+public class Question {
+    String question;
+    boolean answer;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public boolean isAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(boolean answer) {
+        this.answer = answer;
+    }
+
+}
